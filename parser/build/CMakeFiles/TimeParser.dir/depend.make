edb3fd8fc20aaefadcbefcb6a305d4f67b6d6e88
SuviLep <c3lesu00@students.oamk.fi>
2025-10-04 21:18:37 +0300

    .gitignore applied

--- parser/build/CMakeFiles/TimeParser.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TimeParser.
-# This may be replaced when dependencies are built.
--- parser/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/GIT_KOULU/sulautettujenjarjestelmienohjelmistokehitys/parser
-# Build directory: C:/GIT_KOULU/sulautettujenjarjestelmienohjelmistokehitys/parser/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test_cases")
